d6ae1b4e9fb0fbc8124bb38ac36fabeef904ecac
Jaren Boone <jboone0052@gmail.com>
2016-11-16 20:01:27 -0500

    add test:watch

--- src/index.test.js
+++ src/index.test.js
@@ -16,7 +16,7 @@ describe('index.html', function () {
     jsdom.env(index, function (err, window) {
       var h1 = window.document.getElementsByTagName('h1')[0];
 
-      expect(h1.innerHTML).to.equal('Hello World1');
+      expect(h1.innerHTML).to.equal('Hello World!');
       done();
       window.close();
     });
